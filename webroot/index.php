927bb6017e3164a871f2733c9bef23eecfd597c1
Banana <banana@mirage>
2019-12-19 21:58:07 +0100

    more init files

---
+++ webroot/index.php
@@ -0,0 +1,10 @@
+<?php
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE
+#
+# You should have received a copy of the
+# COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Version 1.0
+# along with this program.  If not, see http://www.sun.com/cddl/cddl.html
+
+# 2019 https://www.bananas-playground.net/projekt/selfpaste
+
