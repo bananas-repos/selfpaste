b49c6884c1e4e4d611885955c639f79eea71df14
Banana <banana@mirage>
2019-12-21 18:23:25 +0100

    updated default files. Updated documentation

--- webroot/config.default.php
+++ webroot/config.default.php
@@ -10,3 +10,12 @@
  * 2019 https://www.bananas-playground.net/projekt/selfpaste
  */
 
+# this is your installation secret. Could be anything.
+# Think of it as a key. Change it often to avoid any abuse.
+define('SELFPASTE_UPLOAD_SECRET','PLEASE CHANGE YOUR SECRET');
+# this is the default storage location. If you decide to move, then make sure
+# to move the included .htaccess with it to protect the direct access
+define('SELFPASTE_UPLOAD_DIR','pasties');
+# those are the allowed file types.
+# Make sure you read the README and documentation!
+define(SELFPASTE_ALLOWED_FILETYPES,'');
