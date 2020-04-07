87ce8d25923542e286d2f5dc378d498af00b6fb8
Banana <mail@bananas-playground.net>
2020-04-07 20:47:56 +0200

    first steps in a c client

---
+++ client/c-client/selfpaste.c
@@ -0,0 +1,18 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE
+ * You should have received a copy of the
+ * COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Version 1.0
+ * along with this program.  If not, see http://www.sun.com/cddl/cddl.html
+ *
+ * 2019 - 2020 https://://www.bananas-playground.net/projekt/selfpaste
+ */
+
+#include <stdio.h>
+
+int main() {
+	// https://www.programmingsimplified.com/c-program-examples
+	printf("Writing comments is very useful.\n");
+
+	return(0);
+}
